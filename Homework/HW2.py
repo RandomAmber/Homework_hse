1fc4d02b1925665659f8aaf14022c6cb472f25c6
victoria tsvetkova <nika.c@mail.ru>
2018-09-30 17:01:55 +0300

    hw2 done

--- Homework/HW2.py
+++ Homework/HW2.py
@@ -4,34 +4,35 @@
 # In[ ]:
 
 
-print ("Введите предложение на русском языке:")
-s = input()
-# Подготавливаем предложение: 
-text = s.split()
+import string
+text = input().split(" ")
+if len(text) < 2:
+    print ("Попробуйте снова")
+    
+punct = ""
 output_text = []
-last_word = []
-ending = []
-for i in text[-1]:
-    if i != "!" and i != "." and i != "?":
-        last_word.append(i)
-    else:
-        ending.append(i)
-text[-1] = text[-2]
-text[-2] = "".join(last_word)
 
-# Создаем словарь:
-abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя .,?!:;"
-inv = "бвгдеёжзийклмнопрстуфхцчшщъыьэюяа .,?!:;"
-lower = list(abc)
-upper = list(abc.upper())
-inv_lower = list(inv)
-inv_upper = list(inv.upper())
-keys = lower + upper
-values = inv_lower + inv_upper
-d = dict(zip(keys, values))
+#Работаем с пунктуацией и изменением порядка слов:
+if text[-2][-1] in string.punctuation:
+    punct = text[-2][-1]
+    text[-2] = text[-2].replace(punct, "")
+last = text[-1]
+znaki = ''.join([last[-i] if last[-i] in string.punctuation else '' for i in range(1,len(last)+1)])
+last = last[:-len(znaki)]
+text[-1],text[-2] = text[-2], last + punct
+text = " ".join(text) + znaki
 
-# Шифруем предложение:
-for letter in s:
-    output_text.append(d[letter])
-print (''.join(output_text)[:-1], ''.join(ending), sep = "", end = "")
+
+#Работа над словарем:
+
+alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+keys = alphabet.upper() + alphabet + string.punctuation + string.whitespace
+values = alphabet.upper()[1:] + alphabet.upper()[0] + alphabet[1:]+ alphabet[0] + string.punctuation + string.whitespace
+shifr = dict(zip(keys, values))
+
+
+#Шифрование:
+for letter in text:
+    output_text.append(shifr[letter])
+print(''.join(output_text), sep = "", end = "")
 
